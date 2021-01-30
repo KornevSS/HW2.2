0fbce1d51bdc9a2e724876251d490fc643454673
S.Kornev <post.kornev@gmail.com>
2021-01-30 17:58:44 +0300

    Adding Constraints

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var colorPanelView: UIView!
+    
+    private var colorPanelColor: UIColor {
+        UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorPanelView.backgroundColor = colorPanelColor
     }
 
+    @IBAction func redSliderAction() {
+        colorPanelView.backgroundColor = colorPanelColor
+        redLabel.text = String(redSlider.value)
+
+    }
+    
+    @IBAction func greenSliderAction() {
+        colorPanelView.backgroundColor = colorPanelColor
+        greenLabel.text = String(greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        colorPanelView.backgroundColor = colorPanelColor
+        blueLabel.text = String(blueSlider.value)
+    }
+    
 
 }
 
